75ae8f1aa61ec4313883dcb72135f435c808fd92
Максим Бокарев <unclemaxm@gmail.com>
2022-05-01 14:34:35 +0400

    fourth commit

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,11 +16,11 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardTest {
 
-    private WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
